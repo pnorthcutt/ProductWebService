80d76a0ab2f11aa13a8ddf0f23cd39bf481531eb
Patrick Northcutt <pjnorth69@gmail.com>
2017-10-06 11:45:43 -0500

    Make web package

--- src/main/java/com/northcutt/product/ws/ProductServiceWebPublisher.java
+++ src/main/java/com/northcutt/product/ws/ProductServiceWebPublisher.java
@@ -1,12 +1,17 @@
 package com.northcutt.product.ws;
 
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 import javax.xml.ws.Endpoint;
 
 /**
  * Created by pnorthcutt on 10/3/2017.
  */
-public class ProductServiceWebPublisher {
-    public static void main(String[] args) {
+@WebListener
+public class ProductServiceWebPublisher implements ServletContextListener {
+
+    public void contextInitialized(ServletContextEvent sce) {
         Endpoint.publish("http://localhost:9999/ws/productService", new ProductServiceImp());
     }
 }
